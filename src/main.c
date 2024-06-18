e9cfcf9289f4d568c0165be479cd5aab01e0353a
Grigoris Pavlakis <grigpavl@ece.auth.gr>
2024-06-18 15:06:06 +0300

    add refs to MPLAB files

--- src/main.c
+++ src/main.c
@@ -7,12 +7,19 @@
 
 
 #include "sam.h"
-
+#include "definitions.h"
 
 int main(void)
 {
+    SYS_Initialize(NULL);
+    SYSTICK_TimerStart();
+
     /* Replace with your application code */
     while (1) 
     {
+        PIO_PinSet(PIO_PIN_PA23);
+        SYSTICK_DelayMs(1000);
+        PIO_PinClear(PIO_PIN_PA23);
+        SYSTICK_DelayMs(1000);
     }
 }
